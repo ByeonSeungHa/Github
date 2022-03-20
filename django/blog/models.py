979981b98c626167044f52ec17ef52a26d9acf50
ByeonSeungHa <a01051205048@gmail.com>
2022-03-20 18:26:42 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- django/blog/models.py
+++ django/blog/models.py
@@ -6,9 +6,13 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    # auther : 추후 작성 예정
     def __str__(self):
         return f'[{self.pk}]{self.title}'
-    # auther : 추후 작성 예정
+
+
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
 
 
