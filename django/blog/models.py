b8dec5fcab5d7806452f62f9d242006df4096dac
ByeonSeungHa <a01051205048@gmail.com>
2022-03-26 20:16:08 +0900

    응용 공부중~

--- django/blog/models.py
+++ django/blog/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+import os
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -14,5 +18,11 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
 
 
--- django/blog/views.py
+++ django/blog/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from . models import Post
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts' : posts,
-        }
-    )
+class PostDetail(DetailView):
+    model = Post
+
+    # template_name = 'blog/index.html'
+
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts' : posts,
+#         }
+#     )
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
